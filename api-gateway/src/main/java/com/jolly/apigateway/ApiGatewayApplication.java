53fa0686425ec91cfdeadfd59f531684f0338575
jolly6 <98voonfusing@gmail.com>
2023-04-22 21:49:14 +0800

    Fixed SecurityConfig by adding missing bean @Configuration

--- api-gateway/src/main/java/com/jolly/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/jolly/apigateway/ApiGatewayApplication.java
@@ -2,6 +2,7 @@ package com.jolly.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class ApiGatewayApplication {
 
--- api-gateway/src/main/java/com/jolly/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/jolly/apigateway/config/SecurityConfig.java
@@ -1,6 +1,7 @@
-package com.jolly.apigateway;
+package com.jolly.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -8,6 +9,7 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
+@Configuration
 public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
